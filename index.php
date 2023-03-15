68d3eb9f77cb50ebd136c7a711d09bee48f648c7
leafrei <86363356+leafrei@users.noreply.github.com>
2023-03-15 15:44:18 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,231 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<!-- Primary Meta Tags -->
+<title>Ja. Nein. Vielleicht.</title>
+<meta name="title" content="Ja. Nein. Vielleicht.">
+<meta name="description" content="Comic von Lea Frei">
+
+	body{
+		background:#fff;
+		margin: 0;
+		padding: 0;
+		overflow-y: hidden;
+	}
+	body { -webkit-touch-callout: none !important; }
+	a { -webkit-user-select: none !important; }
+
+	
+	#comicstrip {
+		top: 0;
+		left: 0;
+		overflow-x: auto;
+    	white-space: nowrap;
+	}
+
+	img{
+		height: 100%;
+		width: auto;
+	}
+
+	</style>
+</head> 
+
+<body>
+
+<div id="comicstrip">
+	<nobr>
+		  <img src="lea_frei_webcomic.jpg"> 
+		  <img src="lea_frei_webcomic2.jpg">
+		  <img src="lea_frei_webcomic3.jpg">
+      <img src="lea_frei_webcomic4.jpg"> 
+		  <img src="lea_frei_webcomic5.jpg">
+		  <img src="lea_frei_webcomic6.jpg">
+		  <img src="lea_frei_webcomic7.jpg"> 
+		  <img src="lea_frei_webcomic8.jpg">
+		  <img src="lea_frei_webcomic9.jpg">
+      <img src="lea_frei_webcomic10.jpg"> 
+		  <img src="lea_frei_webcomic11.jpg">
+		  <img src="lea_frei_webcomic12.jpg">
+		  <img src="lea_frei_webcomic13.jpg"> 
+		  <img src="lea_frei_webcomic14.jpg">
+		  <img src="lea_frei_webcomic15.jpg">
+      <img src="lea_frei_webcomic16.jpg"> 
+		  <img src="lea_frei_webcomic17.jpg">
+		  <img src="lea_frei_webcomic18.jpg">
+		  <img src="lea_frei_webcomic19.jpg"> 
+		  <img src="lea_frei_webcomic20.jpg">
+		  <img src="lea_frei_webcomic21.jpg">
+      <img src="lea_frei_webcomic22.jpg"> 
+		  <img src="lea_frei_webcomic23.jpg">
+		  <img src="lea_frei_webcomic24.jpg">
+		  <img src="lea_frei_webcomic25.jpg"> 
+		  <img src="lea_frei_webcomic26.jpg">
+		  <img src="lea_frei_webcomic27.jpg">
+      <img src="lea_frei_webcomic28.jpg"> 
+		  <img src="lea_frei_webcomic29.jpg">
+		  <img src="lea_frei_webcomic30.jpg">
+		  <img src="lea_frei_webcomic31.jpg"> 
+		  <img src="lea_frei_webcomic32.jpg">
+		  <img src="lea_frei_webcomic33.jpg">
+      <img src="lea_frei_webcomic34.jpg"> 
+		  <img src="lea_frei_webcomic35.jpg">
+		  <img src="lea_frei_webcomic36.jpg">
+		  <img src="lea_frei_webcomic37.jpg"> 
+		  <img src="lea_frei_webcomic38.jpg">
+		  <img src="lea_frei_webcomic39.jpg">
+      <img src="lea_frei_webcomic40.jpg"> 
+		  <img src="lea_frei_webcomic41.jpg">
+		  <img src="lea_frei_webcomic42.jpg">
+		  <img src="lea_frei_webcomic43.jpg"> 
+		  <img src="lea_frei_webcomic44.jpg">
+		  <img src="lea_frei_webcomic45.jpg">
+      <img src="lea_frei_webcomic46.jpg"> 
+		  <img src="lea_frei_webcomic47.jpg">
+		  <img src="lea_frei_webcomic48.jpg">
+		  <img src="lea_frei_webcomic49.jpg"> 
+		  <img src="lea_frei_webcomic50.jpg">
+		  <img src="lea_frei_webcomic51.jpg">
+      <img src="lea_frei_webcomic52.jpg"> 
+		  <img src="lea_frei_webcomic53.jpg">
+		  <img src="lea_frei_webcomic54.jpg">
+		  <img src="lea_frei_webcomic55.jpg"> 
+		  <img src="lea_frei_webcomic56.jpg">
+		  <img src="lea_frei_webcomic57.jpg">
+      <img src="lea_frei_webcomic58.jpg"> 
+		  <img src="lea_frei_webcomic59.jpg">
+		  <img src="lea_frei_webcomic60.jpg"> 
+		  <img src="lea_frei_webcomic61.jpg">
+		  <img src="lea_frei_webcomic62.jpg">
+		  <img src="lea_frei_webcomic63.jpg"> 
+		  <img src="lea_frei_webcomic64.jpg">
+		  <img src="lea_frei_webcomic65.jpg">
+      <img src="lea_frei_webcomic66.jpg"> 
+		  <img src="lea_frei_webcomic67.jpg">
+		  <img src="lea_frei_webcomic68.jpg">
+		  <img src="lea_frei_webcomic69.jpg"> 
+		  <img src="lea_frei_webcomic70.jpg">
+		  <img src="lea_frei_webcomic71.jpg"> 
+		  <img src="lea_frei_webcomic72.jpg">
+		  <img src="lea_frei_webcomic73.jpg"> 
+		  <img src="lea_frei_webcomic74.jpg">
+		  <img src="lea_frei_webcomic75.jpg">
+      <img src="lea_frei_webcomic76.jpg"> 
+		  <img src="lea_frei_webcomic77.jpg">
+		  <img src="lea_frei_webcomic78.jpg">
+		  <img src="lea_frei_webcomic79.jpg"> 
+		  <img src="lea_frei_webcomic80.jpg">
+      <img src="lea_frei_webcomic81.jpg"> 
+		  <img src="lea_frei_webcomic82.jpg">
+		  <img src="lea_frei_webcomic83.jpg"> 
+		  <img src="lea_frei_webcomic84.jpg">
+		  <img src="lea_frei_webcomic85.jpg">
+      <img src="lea_frei_webcomic86.jpg"> 
+		  <img src="lea_frei_webcomic87.jpg">
+		  <img src="lea_frei_webcomic88.jpg">
+		  <img src="lea_frei_webcomic89.jpg"> 
+		  <img src="lea_frei_webcomic90.jpg">
+		  <img src="lea_frei_webcomic91.jpg"> 
+		  <img src="lea_frei_webcomic92.jpg">
+		  <img src="lea_frei_webcomic93.jpg"> 
+		  <img src="lea_frei_webcomic94.jpg">
+		  <img src="lea_frei_webcomic95.jpg">
+      <img src="lea_frei_webcomic96.jpg"> 
+		  <img src="lea_frei_webcomic97.jpg">
+		  <img src="lea_frei_webcomic98.jpg">
+		  <img src="lea_frei_webcomic99.jpg"> 
+		  <img src="lea_frei_webcomic100.jpg">
+		  <img src="lea_frei_webcomic101.jpg"> 
+		  <img src="lea_frei_webcomic102.jpg">
+		  <img src="lea_frei_webcomic103.jpg"> 
+		  <img src="lea_frei_webcomic104.jpg">
+		  <img src="lea_frei_webcomic105.jpg">
+      <img src="lea_frei_webcomic106.jpg"> 
+		  <img src="lea_frei_webcomic107.jpg">
+		  <img src="lea_frei_webcomic108.jpg">
+		  <img src="lea_frei_webcomic109.jpg"> 
+		  <img src="lea_frei_webcomic111.jpg">
+		  <img src="lea_frei_webcomic112.jpg">
+		  <img src="lea_frei_webcomic113.jpg">
+		  <img src="lea_frei_webcomic114.jpg">
+		  <img src="lea_frei_webcomic115.jpg">
+		  <img src="lea_frei_webcomic116.jpg">
+		 <img src="lea_frei_webcomic117.jpg">
+		  <img src="lea_frei_webcomic118.jpg">
+		  <img src="lea_frei_webcomic119.jpg"> 
+		  <img src="lea_frei_webcomic120.jpg">
+		  <img src="lea_frei_webcomic121.jpg">
+      <img src="lea_frei_webcomic122.jpg"> 
+		  <img src="lea_frei_webcomic123.jpg">
+		  <img src="lea_frei_webcomic124.jpg">
+		  <img src="lea_frei_webcomic125.jpg"> 
+		  <img src="lea_frei_webcomic126.jpg">
+		  <img src="lea_frei_webcomic127.jpg">
+      <img src="lea_frei_webcomic128.jpg"> 
+		  <img src="lea_frei_webcomic129.jpg">
+		  <img src="lea_frei_webcomic130.jpg">
+		  <img src="lea_frei_webcomic131.jpg"> 
+		  <img src="lea_frei_webcomic132.jpg">
+		  <img src="lea_frei_webcomic133.jpg">
+      <img src="lea_frei_webcomic134.jpg"> 
+		  <img src="lea_frei_webcomic135.jpg">
+		  <img src="lea_frei_webcomic136.jpg">
+		  <img src="lea_frei_webcomic137.jpg"> 
+		  <img src="lea_frei_webcomic138.jpg">
+		  <img src="lea_frei_webcomic139.jpg">
+      <img src="lea_frei_webcomic140.jpg"> 
+		  <img src="lea_frei_webcomic141.jpg">
+		  <img src="lea_frei_webcomic142.jpg">
+		  <img src="lea_frei_webcomic143.jpg"> 
+		  <img src="lea_frei_webcomic144.jpg">
+		  <img src="lea_frei_webcomic145.jpg">
+      <img src="lea_frei_webcomic146.jpg"> 
+		  <img src="lea_frei_webcomic147.jpg">
+		  <img src="lea_frei_webcomic148.jpg">
+		  <img src="lea_frei_webcomic149.jpg"> 
+		  <img src="lea_frei_webcomic150.jpg">
+		  <img src="lea_frei_webcomic151.jpg">
+      <img src="lea_frei_webcomic152.jpg"> 
+		  <img src="lea_frei_webcomic153.jpg">
+		  <img src="lea_frei_webcomic154.jpg">
+		  <img src="lea_frei_webcomic155.jpg"> 
+		  <img src="lea_frei_webcomic156.jpg">
+		  <img src="lea_frei_webcomic157.jpg">
+      <img src="lea_frei_webcomic158.jpg"> 
+		  <img src="lea_frei_webcomic159.jpg">
+		  <img src="lea_frei_webcomic160.jpg"> 
+		  <img src="lea_frei_webcomic161.jpg">
+		  <img src="lea_frei_webcomic162.jpg">
+		  <img src="lea_frei_webcomic163.jpg"> 
+		  <img src="lea_frei_webcomic164.jpg">
+		  <img src="lea_frei_webcomic165.jpg">
+      <img src="lea_frei_webcomic166.jpg"> 
+		  <img src="lea_frei_webcomic167.jpg">
+		  <img src="lea_frei_webcomic168.jpg">
+		  <img src="lea_frei_webcomic169.jpg"> 
+		  <img src="lea_frei_webcomic170.jpg">
+		  <img src="lea_frei_webcomic171.jpg"> 
+		  <img src="lea_frei_webcomic172.jpg">
+		  <img src="lea_frei_webcomic173.jpg"> 
+		  <img src="lea_frei_webcomic174.jpg">
+		  <img src="lea_frei_webcomic175.jpg">
+      <img src="lea_frei_webcomic176.jpg"> 
+		  <img src="lea_frei_webcomic177.jpg">
+		  <img src="lea_frei_webcomic178.jpg">
+		  <img src="lea_frei_webcomic179.jpg"> 
+		  <img src="lea_frei_webcomic180.jpg">
+      <img src="lea_frei_webcomic181.jpg"> 
+		  <img src="lea_frei_webcomic182.jpg">
+		  <img src="lea_frei_webcomic183.jpg"> 
+		  <img src="lea_frei_webcomic184.jpg">
+		  <img src="lea_frei_webcomic185.jpg">
+      <img src="lea_frei_webcomic186.jpg"> 
+		  <img src="lea_frei_webcomic187.jpg">
+		  <img src="lea_frei_webcomic188.jpg">
+		  <img src="lea_frei_webcomic189.jpg"> 
+		  <img src="lea_frei_webcomic190.jpg">
+	</nobr>
+</div> 
+
+</body>
+</html>
